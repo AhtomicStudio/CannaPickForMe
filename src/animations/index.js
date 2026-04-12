4fac38c085d01f47fc85b91ebf6aab5b1c4f936a
AhtomicStudio <twotales89@gmail.com>
2026-04-11 21:03:14 -0700

    feat: add Notes in a Box weighing animation

--- src/animations/index.js
+++ src/animations/index.js
@@ -1,11 +1,13 @@
 import { scalesAnimation }   from './scales.js';
 import { eightBallAnimation } from './eightball.js';
 import { plinkoAnimation }    from './plinko.js';
+import { boxAnimation }       from './box.js';
 
 export const ANIMATIONS = [
   scalesAnimation,
   eightBallAnimation,
   plinkoAnimation,
+  boxAnimation,
 ];
 
 export function pickAnimation() {
